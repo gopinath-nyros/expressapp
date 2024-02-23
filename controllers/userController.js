c8b2a6ed6506c66398dc59acb5c66f3ed11cf7c6
gopinath-nyros <77616467+gopinath-nyros@users.noreply.github.com>
2024-02-23 11:50:56 +0530

    init commit

---
+++ controllers/userController.js
@@ -0,0 +1,21 @@
+const getProfile = (req, res) => {
+  // some logic
+  res.status(200).json({
+    id: "123",
+    firstName: "John",
+    lastName: "Doe",
+  });
+};
+
+const updateProfile = (req, res) => {
+    // some logic
+    res.status(200).json({
+      id: "123",
+      firstName: "Tom",
+      lastName: "Williams",
+    });
+  };
+
+
+
+export {getProfile, updateProfile}
